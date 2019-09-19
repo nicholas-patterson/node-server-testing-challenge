5f74cca62a75bd6734efaad13de09eb1c0c1d89c
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-09-19 15:30:06 -0400

    :sparkles: added player router
    added player router

---
+++ players/playerRouter.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const router = express.Router();
+
+const Players = require("./player-helpers");
+
+router.get("/", (req, res) => {
+  Players.find()
+    .then(players => {
+      res.status(200).json(players);
+    })
+    .catch(error => {
+      res.status(500).json({ error: "Server could not get list of players" });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  Players.findById(id)
+    .then(player => {
+      res.status(200).json(player);
+    })
+    .catch(error => {
+      res.status(500).json({ error: "Server could not get list of players" });
+    });
+});
+
+router.post("/", (req, res) => {
+  const newPlayer = req.body;
+  Players.add(newPlayer)
+    .then(player => {
+      res.status(201).json(player);
+    })
+    .catch(error => {
+      res.status(500).json({ error: "Server could not get list of players" });
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  Players.remove(id)
+    .then(deleted => {
+      res.status(202).json(deleted);
+    })
+    .catch(error => {
+      res.status(500).json({ error: "Server could not get list of players" });
+    });
+});
+
+module.exports = router;
