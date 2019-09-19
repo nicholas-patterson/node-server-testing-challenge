f6f5aaedcfe15eac47b29ae805ff19c34ffdf0fa
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-09-19 15:19:21 -0400

    :sparkles: knexfile setup and db config file
    knexfile setup and db config file

--- api/server.js
+++ api/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const playerRouter = require("../players/playerRouter");
 const server = express();
 
 server.use(express.json());
+server.use("/players", playerRouter);
 
 module.exports = server;
